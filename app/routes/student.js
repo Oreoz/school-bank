898e25be5fd2f012a8a3958030acebf8a85b23bf
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2016-11-25 20:58:35 -0500

    Persisting edited students

--- app/routes/student.js
+++ app/routes/student.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('student', params.student_id);
+  },
+  actions: {
+    save(student) {
+      student.save().then(() => {
+        this.transitionTo('students');
+      })
+    }
   }
 });
